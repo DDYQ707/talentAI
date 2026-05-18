0659f4c1cbe40d40a72d565cb80a43599d1254af
dyq <3280599907@qq.com>
2026-05-18 09:40:46 +0800

    重构后端微服务架构

--- talent-ai-backend/talent-core-biz/src/main/java/com/talent/Main.java
+++
@@ -1,7 +0,0 @@
-package com.talent;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ talent-ai-backend/talent-interview/src/main/java/com/talent/interview/Main.java
@@ -0,0 +1,7 @@
+package com.talent.interview;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
